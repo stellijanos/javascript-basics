b6cb91db668ee5f373569ada200213923ec9b533
Janos Stelli <stellijanos23@gmail.com>
2024-05-26 12:11:46 +0300

    17. Arithmetic Operators

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 // types of operators:
 // arithmetic, assignment, comparison, logical, bitwise
+
+// 1. Arithmetic Operators
+
+let x = 10;
+let y = 3; 
+ 
+console.log(x + y); // addition
+console.log(x - y); // subtraction
+console.log(x * y); // multiplication
+console.log(x / y); // division
+console.log(x % y); // remainder of division (modulo)
+console.log(x ** y); // exponentiation
+
+
+// Increment (++)
+// console.log(++x); // value of x incremented first, then we see the value of x in the console
+// console.log(x++); // we see the value of x in the console, then the value of x is incremented
+// console.log(x);
+
+// Decrement (--) 
+console.log(--x); // x = x - 1 then print out x
+console.log(x--); // print out x then x = x - 1
+console.log(x);
