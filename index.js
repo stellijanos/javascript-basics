e83d8f0008db7f0b2c342c0d2868cdc72d27c65c
Janos Stelli <stellijanos23@gmail.com>
2024-05-26 15:59:26 +0300

    30.3 - For

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
-let role = 'guest';
 
-switch(role) {
-    case 'guest':
-        console.log('Guest User');
-        break;
-        
-    case 'moderator':
-        console.log('Moderator User');
-        break;
-
-    default:
-        console.log('Unknown role');
+for (let i = 5; i >= 0; i--) {
+    if (i % 2 !== 0) console.log(i);
 }
-
-if (role === 'guest') console.log('Guest');
-else if (role === 'moderator') console.log('Moderator');
-else console.log('Unknown');
-
