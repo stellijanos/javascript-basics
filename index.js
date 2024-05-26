357ecc72279b7af061752c81ea4f25514441d16b
Janos Stelli <stellijanos23@gmail.com>
2024-05-26 18:16:18 +0300

    46.19 - Exercise-Stars

--- index.js
+++ index.js
@@ -1,30 +1,10 @@
 
-const marks = [80, 90, 50];
+showStars(5);
 
-// Average = 70
-// 0- 59 : F
-// 60- 69: D
-// 70-79: C
-// 80-89: B
-// 90-100: A
-
-console.log(calculateGrade(marks));
-
-
-
-function calculateGrade(marks) {
-    
-    const average = calculateAverage(marks);
-    if (average < 60) return 'F';
-    if (average < 70) return 'D';
-    if (average < 80) return 'C';
-    if (average < 90) return 'B';
-    return 'A';
-}
-
-function calculateAverage(array) {
-    let sum = 0;
-    for (let num of array) 
-        sum += num;
-    return sum / array.length;
+function showStars(rows) {
+    let row = '*';
+    for (let i = 0; i < rows; i++) {
+        console.log(row);
+        row += '*';
+    }
 }
