fa34c934a0526490bdcd4d1d492ea604640a7d86
Janos Stelli <stellijanos23@gmail.com>
2024-05-26 11:51:09 +0300

    14. Functions

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-let selectedColors = ['red', 'blue'];
-selectedColors[2] = 1;
-console.log(selectedColors.length);
+function greet(name, lastName) { // function declaration (here to declare parameters)
+    console.log('Hello ' + name + ' ' + lastName);
+}
+
+greet('Janos', 'Stelli'); // function calling (here to pass arguments)
+
+// parameter at time of delcaration
+// argument is the actual value of supply for that paramter 
+
