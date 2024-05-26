47ff31bb77da75a322dbfe8f230e37c057de354e
Janos Stelli <stellijanos23@gmail.com>
2024-05-26 12:42:43 +0300

    20.5 - Equality Operators

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 
-let x = 1;
+// Strict Equality Operator (Type + Value)
+console.log(1 === 1);
+console.log('1' === 1);
 
-// Relational
-console.log(x > 0);
-console.log(x >= 1);
-console.log(x < 1);
-console.log(x <= 1);
+// Lose Equality Operator (Value)
+console.log(1 == 1);
+console.log('1' == 1);
 
-// Equality
-console.log(x === 1);
-console.log(x !== 1);
+console.log(true == 1);
