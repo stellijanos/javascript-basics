83529507ea998befc7fea682dcdd45a302a3656e
Janos Stelli <stellijanos23@gmail.com>
2024-05-26 17:34:30 +0300

    41.14 - Exercise-Even and Odd Numbers

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
 
-// Speed limit = 70
-// 5 -> 1 point 
-// Math.floor(1.3)
-// 12 points -> suspended
+showNumbers(10);
 
-checkSpeed(80);
-
-function checkSpeed(speed) {
-
-    const speedLimit = 70;
-    const kmPerPoint = 5;
-    const points = Math.floor((speed - speedLimit)/kmPerPoint);
-    
-    if (points <= 0) console.log('Ok');
-    else if (points >= 12) {
-        console.log('License suspended');
-    } else {
-        console.log('Points', points);
+function showNumbers(limit) {
+    for (let i = 0 ; i <=limit; i++) {
+        const message = (i % 2 === 0) ? 'EVEN' : 'ODD';
+        console.log(i,message);
     }
 }
