3322230bffe37352da8fa329c5c6a9a77a9f71f3
Janos Stelli <stellijanos23@gmail.com>
2024-05-26 11:31:34 +0300

    11. 4 - Dynamic Typing

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ let age = 20; // Number Literal
 let isApproved = false; // Boolean Literal 
 let firstName = undefined; // by default every unassigned variable has undefined value
 let selectedColor = null; // we use null, if we want to clear the value of a variable
+
+// static language - type of variable remains the same during runtime
+// dynamic typing (language) - type of variable can change during the runtime
+ 
+// undefined is a type but also a value
+// null value has type 'object'
