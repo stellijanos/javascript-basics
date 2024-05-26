507acb381e075752497fdd0b421c4f2dc8898772
Janos Stelli <stellijanos23@gmail.com>
2024-05-26 16:06:37 +0300

    32.5 - Do...While

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 
-for (let i = 5; i >= 0; i--) {
-    if (i % 2 !== 0) console.log(i);
-}
+// for (let i = 5; i >= 0; i--) {
+//     if (i % 2 !== 0) console.log(i);
+// }
 
-let i = 0;
-while (i <= 5) {
+// let i = 9;
+// while (i <= 5) {
+//     if (i % 2 !== 0) console.log(i);
+//     i++;
+// }
+
+// do-while 
+
+let i = 9;
+
+do {
     if (i % 2 !== 0) console.log(i);
     i++;
-}
+} while(i<= 5);
