c9f218943a03c4729898ddf902780a158b2af87e
Janos Stelli <stellijanos23@gmail.com>
2024-05-26 17:01:48 +0300

    39.12 - Exercise-FizzBuzz

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 
-console.log(isLandScape(300, 600));
+const output = fizzBuzz(false);
+console.log(output);
 
-function isLandScape(width, height) {
-    return width > height;
+function fizzBuzz(input) {
+    if (typeof input !== 'number') return NaN;
+    if (input % 15 === 0) return 'FizzBuzz';
+    if (input % 3 === 0) return 'Fizz';
+    if (input % 5 === 0) return 'Buzz';
+    return input;
 }
