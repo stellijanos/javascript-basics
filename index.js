68677991cb6b638a8131f14e60738f6dba06391a
Janos Stelli <stellijanos23@gmail.com>
2024-05-26 12:04:41 +0300

    16.1 - JavaScript Operators

--- index.js
+++ index.js
@@ -1,21 +1,2 @@
-
-// Performing a task
-function greet(name, lastName) { // function declaration (here to declare parameters)
-    console.log('Hello ' + name + ' ' + lastName);
-}
-
-// greet('Janos', 'Stelli'); // function calling (here to pass arguments)
-
-// parameter at time of delcaration
-// argument is the actual value of supply for that paramter 
-
-
-// Calculating a value
-function square(number) {
-    return number * number;
-}
-
-console.log(square(2));
-
-// a function is a set of statements that either performs a task, or calculates and returns a value
-
+// types of operators:
+// arithmetic, assignment, comparison, logical, bitwise
