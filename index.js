b4b0067d378807b600ceed9b98df491b8db4987a
Janos Stelli <stellijanos23@gmail.com>
2024-05-26 12:14:09 +0300

    18.3 - Assignment Operatos

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
-// types of operators:
-// arithmetic, assignment, comparison, logical, bitwise
-
-// 1. Arithmetic Operators
 
 let x = 10;
-let y = 3; 
- 
-console.log(x + y); // addition
-console.log(x - y); // subtraction
-console.log(x * y); // multiplication
-console.log(x / y); // division
-console.log(x % y); // remainder of division (modulo)
-console.log(x ** y); // exponentiation
 
+x++;
+x = x + 5;
+x += 5; // addition assignemnt Operator
 
-// Increment (++)
-// console.log(++x); // value of x incremented first, then we see the value of x in the console
-// console.log(x++); // we see the value of x in the console, then the value of x is incremented
-// console.log(x);
+x = x * 3 
+x *= 3 // multiplication assignment Operator
 
-// Decrement (--) 
-console.log(--x); // x = x - 1 then print out x
-console.log(x--); // print out x then x = x - 1
-console.log(x);
