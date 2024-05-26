613656cfe7e74c020f776e4ead6247159ea9fc9b
Janos Stelli <stellijanos23@gmail.com>
2024-05-26 11:21:09 +0300

    8.1 - Variables

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-// This is my first JavaScript code!
-console.log('Hello World');
+
+let name = 'Janos';
+console.log(name); // by default all variables are 'undefined'
+
+// cannot be a reserved keyword
+// should be meaningful
+// cannot start with a number (1name - not ok)
+// cannot contain a space or hyphen (-) 
+// are case sensitive
+
+let firstName = 'Janos';  // camel notation (case)
+let lastname = 'Stelli';
