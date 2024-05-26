948ebd9b6b8311d9119a6573594fd80fd2373ceb
Janos Stelli <stellijanos23@gmail.com>
2024-05-26 18:09:51 +0300

    45.18 - Exercise-Grade

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 
-console.log(sum(10));
+const marks = [80, 90, 50];
 
-function sum(limit) {
-    let sum = 0;
+// Average = 70
+// 0- 59 : F
+// 60- 69: D
+// 70-79: C
+// 80-89: B
+// 90-100: A
+
+console.log(calculateGrade(marks));
+
+
+
+function calculateGrade(marks) {
     
-    for (let number = 0; number <= limit; number++)
-        if (number % 3 === 0 || number % 5 === 0 ) 
-            sum+= number;
+    const average = calculateAverage(marks);
+    if (average < 60) return 'F';
+    if (average < 70) return 'D';
+    if (average < 80) return 'C';
+    if (average < 90) return 'B';
+    return 'A';
+}
 
-    return sum;
+function calculateAverage(array) {
+    let sum = 0;
+    for (let num of array) 
+        sum += num;
+    return sum / array.length;
 }
