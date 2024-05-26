1998fd2f7c8c070cafb9962484a785c956aafffe
Janos Stelli <stellijanos23@gmail.com>
2024-05-26 11:27:21 +0300

    10. 3- Primitive Types

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 
-const interestRate = 0.3;
-interestRate = 1;
-console.log(interestRate);
+let name = 'Janos'; // String Literal
+let age = 20; // Number Literal
+let isApproved = false; // Boolean Literal 
+let firstName = undefined; // by default every unassigned variable has undefined value
+let selectedColor = null; // we use null, if we want to clear the value of a variable
