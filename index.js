08adfa4250fb17149e3833027dcbb51790481a01
Janos Stelli <stellijanos23@gmail.com>
2024-05-26 18:24:20 +0300

    47.20 - Exercise-Prime Numbers

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 
-showStars(5);
+showPrimes(100);
 
-function showStars(rows) {
-    let row = '*';
-    for (let i = 0; i < rows; i++) {
-        console.log(row);
-        row += '*';
-    }
+
+function showPrimes(limit) {
+    for (let num = 2; num <= limit; num++) 
+        if (isPrime(num)) console.log(num);
+}
+
+function isPrime(number) {
+    if (number <= 1 ) return false;
+    if (number <= 3) return true;
+    if (number % 2 === 0 || number % 3 === 0) return false;
+
+    const integerSqrt = Math.trunc(Math.sqrt(number));
+
+    for (let divisor = 5; divisor <= integerSqrt; divisor += 2)
+        if (number % divisor === 0) 
+            return false;
+
+    return true;
 }
