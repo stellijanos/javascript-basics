9e1dda5da993cab29c1a2aae0ceb38c38167038f
Janos Stelli <stellijanos23@gmail.com>
2024-05-26 11:23:22 +0300

    9.2 - Constants

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
 
-let name = 'Janos';
-console.log(name); // by default all variables are 'undefined'
-
-// cannot be a reserved keyword
-// should be meaningful
-// cannot start with a number (1name - not ok)
-// cannot contain a space or hyphen (-) 
-// are case sensitive
-
-let firstName = 'Janos';  // camel notation (case)
-let lastname = 'Stelli';
+const interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
