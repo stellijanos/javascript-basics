daf6dddad0f2febd5c6d669522bc22d7fec33f63
Janos Stelli <stellijanos23@gmail.com>
2024-05-26 15:43:02 +0300

    28.1 - If...else

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-let a = 'red';
-let b = 'blue';
 
-[a, b] = [b,a];
+// Hour 
+// if hour is between 6am and 12pm: Good morning!
+// if it is between 12pm and 6pm: Good afternoon!
+// Otherwise: Good evenning! 
 
-console.log(a);
-console.log(b);
+let hour = 20; 
+
+if (hour >= 6 && hour < 12) 
+    console.log('Good Morning');
+else if (hour >= 12 && hour < 18) 
+    console.log('Good afternnon');
+else 
+    console.log('Good evening');
