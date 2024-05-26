5661eb92b2d9f85b006a0da85bb3af6117a6d67e
Janos Stelli <stellijanos23@gmail.com>
2024-05-26 13:10:31 +0300

    25.10 - Operator Precedence

--- index.js
+++ index.js
@@ -1,24 +1,2 @@
-
-// 1 = 00000001
-// 2 = 00000010
-// 3 = 00000011
-// R = 00000000
-
-// Read, Write, Execute
-// 00000100 - only read
-// 00000110 - only read and write
-// 00000111 - all 3 permissions
-
-console.log( 1 | 2); // Bitwise OR
-console.log( 1 & 2); // Bitwise AND
-
-const readPermission = 4;
-const writePermission = 2;
-const executePermission = 1;
-
-let myPermission = 0;
-myPermission = myPermission | writePermission;
-console.log(myPermission);
-
-let message = (myPermission & readPermission) ? 'yes' : 'no';
-console.log(message);
+let x = (2 + 3) * 4;
+console.log(x);
