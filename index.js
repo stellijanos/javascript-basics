24a8923c6bd25d5ffc43a19737863e083564714c
Janos Stelli <stellijanos23@gmail.com>
2024-05-27 06:26:24 +0300

    Value vs. Reference Types

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
+// Value Types
+// Number, String, Boolea, Symbol, undefined, null
 
-function Circle(radius) {
-    this.radius = radius;
-    this.draw = function() {
-        console.log('draw');
-    }
-}
+// Reference Types
+// Object Function Array
 
-Circle.call({}, 1);
-Circle.apply({}, [1,2,3]);
+let x = {value: 10};
+let y = x;
 
-const another = new Circle(1);
+x.value = 20;
+// the object name (address) is passed by reference
+// x and y are pointing to the same object in the memory
 
+// Primitives are copied by their VALUE
+// Objects are copied by their REFERENCE
 
-// functions are Objects
+let obj = {value: 10};
+
+function increase(number) { // 2 independent copies
+    number.value++;
+} 
+
+increase(obj);
+
+console.log(obj);
