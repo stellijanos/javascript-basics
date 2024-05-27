fe34a9aa67d4accec1c0da45454d24b5024c9f1d
Janos Stelli <stellijanos23@gmail.com>
2024-05-27 07:44:22 +0300

    Blog Post Object

--- index.js
+++ index.js
@@ -1,38 +1,22 @@
 
-let address1 =  new Address('a', 'b', 'c');
-let address2 = new Address('a', 'b', 'c');
-let address3 = address1;
+// title 
+// body 
+// author 
+// views 
+// comments 
+//    (author, body)
+// isLive
 
-console.log(areEqual(address1, address2));
-console.log(areSame(address1, address2));
-console.log(areSame(address1, address3));
-
-
-
-// Constructor Function 
-function Address(street, city, zipCode) {
-    this.street = street;
-    this.city = city;
-    this.zipCode = zipCode;
+const post = {
+    title: 'a',
+    body: 'b',
+    author: 'c',
+    views: 10,
+    comments: [
+        {author: 'a', body: 'b'},
+        {author: 'c', body: 'd'}
+    ],
+    isLive: false
 }
 
-
-function areEqual(address1, address2) {
-    for (key in address1) 
-        if (address1[key] !== address2[key])
-            return false;
-    return true;
-}
-
-// OR
-// function areEqual(add1, add2) {
-//     return address1.street === add2.street &&
-//         add1.city === add2.city && 
-//         add1.zipCode === add2.zipCode;
-// }
-
-
-
-function areSame(address1, address2) {
-    return address1 === address2;
-}
+console.log(post);
