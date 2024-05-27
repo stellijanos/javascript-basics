090c0bd19b6fe485128697174e5139249f63d58f
Janos Stelli <stellijanos23@gmail.com>
2024-05-27 05:53:02 +0300

    Basics

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 
-showPrimes(100);
+// Object-oriented Programming (OOP)
 
-
-function showPrimes(limit) {
-    for (let num = 2; num <= limit; num++) 
-        if (isPrime(num)) console.log(num);
+const circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1
+    },
+    isVisible: true,
+    draw: function() {
+        console.log('draw');
+    },
+    move: function() {}
 }
 
-function isPrime(number) {
-    if (number <= 1 ) return false;
-    if (number <= 3) return true;
-    if (number % 2 === 0 || number % 3 === 0) return false;
+ // Purpose of an Object is to group related variables or functions
 
-    const integerSqrt = Math.trunc(Math.sqrt(number));
-
-    for (let divisor = 5; divisor <= integerSqrt; divisor += 2)
-        if (number % divisor === 0) 
-            return false;
-
-    return true;
-}
+circle.draw(); // draw method (function is part of an object -> method)
