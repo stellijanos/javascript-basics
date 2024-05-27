0cf41a3c04ab161397bae616dde802e03c9e598f
Janos Stelli <stellijanos23@gmail.com>
2024-05-27 06:59:34 +0300

    Garbage Collection

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
 
-const circle = {
-    radius: 1,
-    draw() {
-        console.log('draw');
-    }
-};
-
-
-// for (let key in circle) {
-//     another[key] = circle[key];
-// }
-
-// const assign = Object.assign({
-//     color:'yellow'
-// }, circle); // cloning circle properties and methods into another
-
-
-const another = {...circle}; // spread the circle Object
-
-console.log(another);
+let circle = {};
+console.log(circle);
