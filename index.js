95e0db5b5504f0735d75f70a1591c24e7cd71d84
Janos Stelli <stellijanos23@gmail.com>
2024-05-27 12:23:17 +0300

    The Spread Operator

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 
-const first = [{id: 1}];
+const first = [1, 2, 3];
 const second = [4, 5, 6];
 
-const combined = first.concat(second);
-first[0].id = 10;
-
-const slice = combined.slice();
+const combined = [...first, 'a' ,...second, 'b'];
 
 console.log(combined);
-console.log(slice);
+
+// const copy = combined.slice();
+const copy = [...combined];
