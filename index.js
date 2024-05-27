4270cc3db41b9f49109a78285b7bea70d7b4877a
Janos Stelli <stellijanos23@gmail.com>
2024-05-27 07:22:28 +0300

    Date

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 
-const name = 'John'
+const now = new Date();
+const date1 = new Date('May 11 2018 09:00');
+const date2 = new Date(2018, 4, 11, 9);
 
-const another = 
-`Hi ${name} ${2+3}, 
-
-Thank you for joining my mailing list.
-
-Regards,
-Janos`;
+now.setFullYear(2023);
