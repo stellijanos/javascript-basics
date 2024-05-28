e9c11e29a5326c31c153a34d37b4bf6fecf94410
Janos Stelli <stellijanos23@gmail.com>
2024-05-28 16:42:13 +0300

    Mapping an Array

--- index.js
+++ index.js
@@ -3,4 +3,22 @@ const numbers = [2, 3, 0, -1, 1];
 
 const filtered = numbers.filter(n => n > 0);
 
-console.log(filtered);
+const items = filtered.map(n => '<li>' + n + '</li>');
+
+const html = '<ul>' + items.join('') + '</ul>';
+// console.log(html);
+
+// const objects = filtered.map(n =>  {value: n}); // undefined elements array
+// js engine thinks that we are defining a function with {}
+const objects = filtered.map(n =>  ({value: n}));
+
+console.log(objects);
+
+
+const result = numbers
+            .filter(n => n > 0)
+            .map(n =>  ({value: n}))
+            .filter(obj => obj.value > 1)
+            .map(obj => obj.value);
+
+console.log(result);
