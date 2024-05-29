43ea6e02a18019d6bba4414b4c544d028fd8744b
Janos Stelli <stellijanos23@gmail.com>
2024-05-29 17:43:11 +0300

    Exercise 1 - Sum of Arguments

--- index.js
+++ index.js
@@ -1,63 +1,10 @@
-// method -> obj
-// function ->global (window in browser, global in node)
+// sum(1, 2, 3, 4) => 10
 
-// const video = {
-//     title: 'a',
-//     tags: ['a', 'b', 'c'],
-//     showTags() {
-//         const self = this; // references the video object
-//         this.tags.forEach(function(tag) {
-//             console.log(self.title, tag);
-          
-//         });
-//     }
-// };
-
-
-// video.showTags();
-
-
-
-
-function playVideo(a, b) {
-    console.log(this);
+function sum(...items) {
+    if(items.length === 1 && Array.isArray(items[0])) {
+        return items[0].reduce((acc, curr) => acc + curr);
+    }
+    return items.reduce((acc, curr) => acc + curr);
 }
 
-// playVideo.call({name: 'Janos'}, 1, 2); // define/override this 
-// playVideo.apply({name: 'Janos'}, [1, 2]); // define/override this 
-// playVideo.bind({name: 'Janos'})(); // returns a new function
-// call and apply difference is, 
-// that in apply we have to pass parameters of a function as array
-// and in call like parameters
-
-// and sets this to point to that object permanantly
-
-// playVideo();
-
-
-
-// const video = {
-//     title: 'a',
-//     tags: ['a', 'b', 'c'],
-//     showTags() {
-//         this.tags.forEach(function(tag) {
-//             console.log(this.title, tag);
-//         }.bind(this)); // to make this refer to the video object
-//     }
-// };
-
-
-const video = {
-    title: 'a',
-    tags: ['a', 'b', 'c'],
-    showTags() {
-        this.tags.forEach(tag => {
-            console.log(this.title, tag);
-        }); // to make this refer to the video object
-    }
-};
-
-
-video.showTags();
-
-// arrowfunctions inherit the 'this' value from the containing function
+console.log(sum([1,2,3,4]));
