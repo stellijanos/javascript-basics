e1265ae6024dc63660e426635e5dd06cc9ce883d
Janos Stelli <stellijanos23@gmail.com>
2024-05-29 17:53:05 +0300

    Exercise 3 - Error Handling

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 
-const circle = {
-    radius: 1,
-    get area() {
-        return Math.PI * this.radius * this.radius;
-    }
-};
 
-console.log(circle.area);
+try {
+    const numbers = [1, 2, 3, 4, 1];
+    const count = countOccurencies(true, 1);
+    console.log(count);
+} 
+catch (e) {
+    console.log(e.message);
+}
+
+function countOccurencies(array, searchElement) {
+
+    if (!Array.isArray(array)) {
+        throw new Error('Provide an array!');
+    }
+
+    return array.reduce((accumlator, current) => {
+        const occurence = (current === searchElement) ? 1 : 0;
+        return accumlator + occurence;
+    }, 0)
+}
