2f2f4f89409931b8c2c2896371f5c476f66ad4c5
Janos Stelli <stellijanos23@gmail.com>
2024-05-29 17:37:31 +0300

    Changing This

--- index.js
+++ index.js
@@ -1,39 +1,63 @@
-// this = the object that is executing the current function
-
 // method -> obj
 // function ->global (window in browser, global in node)
+
+// const video = {
+//     title: 'a',
+//     tags: ['a', 'b', 'c'],
+//     showTags() {
+//         const self = this; // references the video object
+//         this.tags.forEach(function(tag) {
+//             console.log(self.title, tag);
+          
+//         });
+//     }
+// };
+
+
+// video.showTags();
+
+
+
+
+function playVideo(a, b) {
+    console.log(this);
+}
+
+// playVideo.call({name: 'Janos'}, 1, 2); // define/override this 
+// playVideo.apply({name: 'Janos'}, [1, 2]); // define/override this 
+// playVideo.bind({name: 'Janos'})(); // returns a new function
+// call and apply difference is, 
+// that in apply we have to pass parameters of a function as array
+// and in call like parameters
+
+// and sets this to point to that object permanantly
+
+// playVideo();
+
+
+
+// const video = {
+//     title: 'a',
+//     tags: ['a', 'b', 'c'],
+//     showTags() {
+//         this.tags.forEach(function(tag) {
+//             console.log(this.title, tag);
+//         }.bind(this)); // to make this refer to the video object
+//     }
+// };
+
 
 const video = {
     title: 'a',
     tags: ['a', 'b', 'c'],
     showTags() {
-        this.tags.forEach(function(tag) {
-            console.log(this.title, tag); // reffers to the window object
-            // its a regular function in foreach
-        }, this); // to override this (window)
+        this.tags.forEach(tag => {
+            console.log(this.title, tag);
+        }); // to make this refer to the video object
     }
-}
-
-
-video.stop = function() {
-    console.log(this); // refers to the video object
-}
-
-// video.play();
-// video.stop();
-
-function playVideo() {
-    console.log(this); // refers to the Window object
-}
-
-// playVideo();
-
-function Video(title) {
-    this.title = title;
-    console.log(this);
-}
-
-const v = new Video('b');
+};
 
 
 video.showTags();
+
+// arrowfunctions inherit the 'this' value from the containing function
