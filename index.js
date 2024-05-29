5e1272f6beb4beea21fc7f13494b8a64a4e53d03
Janos Stelli <stellijanos23@gmail.com>
2024-05-29 11:51:22 +0300

    Try and Catch

--- index.js
+++ index.js
@@ -6,14 +6,28 @@ const person = {
         return `${person.firstname} ${person.lastname}`;
     },
     set fullname(value) {
+
+        if (typeof value !== 'string') 
+            throw new Error('Value is not a string');
+
         const parts = value.split(' ');
+        if (parts.length !== 2) 
+            throw new Error('Enter a first and last name');
         this.firstname = parts[0];
         this.lastname = parts[1];
     }
 };
 
-// getters => access properties
-// setters => change (mutate) them
+// defensive programming = error handling at the beginning
+// ensuring that the values are valid
+// throwing error is an exception
+// consoling error is error
 
-person.fullname = 'John Smith';
+try {
+    person.fullname = '';
+} 
+catch (e) {
+    alert(e);
+}
+
 console.log(person);
