4e13d25b722579686a747edaf99c966dbf94a397
Janos Stelli <stellijanos23@gmail.com>
2024-05-29 11:56:53 +0300

    Local vs. Global Scope

--- index.js
+++ index.js
@@ -1,33 +1,18 @@
 
-const person = {
-    firstname: 'Janos',
-    lastname: 'Stelli',
-    get fullname() {
-        return `${person.firstname} ${person.lastname}`;
-    },
-    set fullname(value) {
+const color = 'red';
 
-        if (typeof value !== 'string') 
-            throw new Error('Value is not a string');
-
-        const parts = value.split(' ');
-        if (parts.length !== 2) 
-            throw new Error('Enter a first and last name');
-        this.firstname = parts[0];
-        this.lastname = parts[1];
-    }
-};
-
-// defensive programming = error handling at the beginning
-// ensuring that the values are valid
-// throwing error is an exception
-// consoling error is error
-
-try {
-    person.fullname = '';
-} 
-catch (e) {
-    alert(e);
+function start() {
+    const message = 'hi';
+    const color = 'blue';
+    console.log(color);
 }
 
-console.log(person);
+function stop() {
+    const message = 'bye';
+}
+
+// console.log(message);
+
+start();
+
+// scope determines where a variable or const is accessible
