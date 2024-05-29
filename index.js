6abcca0df41fbd06a12363d786d597babf426e35
Janos Stelli <stellijanos23@gmail.com>
2024-05-29 11:16:05 +0300

    Arguments

--- index.js
+++ index.js
@@ -1,28 +1,10 @@
 
+function sum() {
+    let total = 0;
 
-walk();
-
-// Function declaration
-
-function walk() {
-    console.log('walk');
+    for (let value of arguments) // can use of in any object with iterator
+        total += value;
+    return total;
 }
 
-// Anonymous Function Expression 
-let run = function() {
-    console.log('run');
-};
-
-// Named Function Expression 
-let swim = function walk() {
-    console.log('swim');
-};
-
-let move = run;
-
-run();
-move();
-
-// variable or constant cannot be used before declared, only function declarations
-
-// hoisting is the process of moving function delcarations to the top of the file
+console.log(sum(1, 2, 3, 4, 5));
