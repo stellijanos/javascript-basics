260eff8d37bb9f005e5304e7d3613bdd4608b0af
Janos Stelli <stellijanos23@gmail.com>
2024-05-29 11:25:28 +0300

    Default Parameters

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 
-function sum(discount, ...prices) { // ... the rest operator here (not/ opposite of the spread operator)
-
-    const total = prices.reduce((a,b) => a + b);
-    return total * (1 - discount);
+function interest(principal, rate = 3.5, years = 5) {
+    return principal * rate / 100 * years;
 }
 
-console.log(sum(0.1, 20, 30));
-
-
-// A rest (of the) parameter must be last in a parameter list.
+console.log(interest(10000));
